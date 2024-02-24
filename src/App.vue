03b09d00425a38b1858e125ff4fe75254e3c7e6c
Мария Рудометова <rudometovamo@student.bmstu.ru>
2024-02-24 03:02:12 +0300

    new a

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import axios from 'axios'
+import axios from 'axios';
 import { resolveTransitionHooks } from 'vue'
 
 export default {
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import dotenv from 'dotenv'
+dotenv.config({path: 'D:/proj_vuejs/weather/.env'});
 
 createApp(App).mount('#app')
