1b0c310fc83d34181250a7ad8ef16535987418f8
Мария Рудометова <rudometovamo@student.bmstu.ru>
2024-02-24 12:39:55 +0300

    create repo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import dotenv from 'dotenv'
-// dotenv.config({path: 'D:/proj_vuejs/weather/.env'});
 
 createApp(App).mount('#app')
